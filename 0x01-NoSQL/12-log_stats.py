a96618e4c019edf1d2dce6dfeed866e3e557c4c2
Samson <samsonmanie2022@gmail.com>
2024-08-12 13:32:11 -0700

    NOSQL

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -10,9 +10,7 @@ def print_nginx_request_logs(nginx_collection):
     print('{} logs'.format(nginx_collection.count_documents({})))
     print('Methods:')
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    for method in methods:
-        req_count = len(list(nginx_collection.find({'method': method})))
-        print('\tmethod {}: {}'.format(method, req_count))
+    for method in methods:print('\tmethod {}: {}'.format(method, req_count))
     status_checks_count = len(list(
         nginx_collection.find({'method': 'GET', 'path': '/status'})
     ))
@@ -28,3 +26,4 @@ def run():
 
 if __name__ == '__main__':
     run()
+
